9d8c8bc31ec68dc8a859046b084915147ed88ac6
beryl-11 <db.beryldong@gmail.com>
2026-05-19 15:09:03 -0400

    added grade

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const courses = [
         name: "Course 1",
         credit : 4,
         semester : "Fall 2025",
+        grade : "A+",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -12,6 +13,7 @@ const courses = [
         name: "Course 2",
         credit : 4,
         semester : "Spring 2026",
+        grade : "A+",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -21,6 +23,7 @@ const courses = [
         name: "Course 3",
         credit : 2,
         semester : "Fall 2025",
+        grade : "A-",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -33,7 +36,7 @@ courses.forEach(course => {
     const courseDiv = document.createElement("section");
 
     courseDiv.innerHTML = `
-    <h2 class="courseName">${course.name}</h2>
+    <h2 class="courseName">${course.name} ${course.grade}</h2>
   `;
 
     courseNames.appendChild(courseDiv);
