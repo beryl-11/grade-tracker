5e188b1566a11af58d7b8b5e207c121578f4b112
beryl-11 <db.beryldong@gmail.com>
2026-05-19 18:47:39 -0400

    restructured html and created subpage

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const courses = [
     {
         name: "Course 1",
-        credit : 4,
-        semester : "Fall 2025",
-        grade : "A+",
+        credit: 4,
+        semester: "Fall 2025",
+        grade: "A+",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -11,9 +11,9 @@ const courses = [
     },
     {
         name: "Course 2",
-        credit : 4,
-        semester : "Spring 2026",
-        grade : "A+",
+        credit: 4,
+        semester: "Spring 2026",
+        grade: "A+",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -21,9 +21,9 @@ const courses = [
     },
     {
         name: "Course 3",
-        credit : 2,
-        semester : "Fall 2025",
-        grade : "A-",
+        credit: 2,
+        semester: "Fall 2025",
+        grade: "A-",
         assignments: [
             { name: "Homework 1", score: 95, max: 100 },
             { name: "Midterm", score: 88, max: 100 }
@@ -33,10 +33,29 @@ const courses = [
 const courseNames = document.getElementById("course-names");
 
 courses.forEach(course => {
-    const courseDiv = document.createElement("section");
+    /* not working but attempting
+    courses.forEach(semester => {
+    const courseDiv1 = document.createElement("div");
+    courseDiv1.innerHTML = `<button>${semester}</button>`;
+    courseNames.appendChild(courseDiv1);
+    print(semester);
+    courses[semester].forEach(course => {
+        const courseDiv = document.createElement("div");
+
+        courseDiv.innerHTML = `
+            <h2 class="courseName"><a href="#${course.name}">${course.name} ${course.grade}</a></h2>
+            `;
+
+        courseNames.appendChild(courseDiv);
+    });
+});*/
+    const courseDiv = document.createElement("table");
 
     courseDiv.innerHTML = `
-    <h2 class="courseName"><a href="#${course.name}">${course.name} ${course.grade}</a></h2>
+    <tr class="courseName">
+        <td><a href="#${course.name}">${course.name}</a></td>
+        <td>${course.grade}</td>
+        </tr>
   `;
 
     courseNames.appendChild(courseDiv);
