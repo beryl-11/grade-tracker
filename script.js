ec7407ce51227b87895f46af3674378f81178ce6
beryl-11 <db.beryldong@gmail.com>
2026-07-03 13:05:49 -0400

    moved courses to new json file

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const courses = {
-    "Fall 2025": [
-        {
-            name: "F25C1", credit: 4, grade: "A+", assignments: [
-                { name: "Homework 1", score: 95, max: 100 },
-                { name: "Midterm", score: 88, max: 100 }
-            ]
-        },
-        {
-            name: "F25C1", credit: 2, grade: "A-", assignments: [
-                { name: "Homework 1", score: 95, max: 100 },
-                { name: "Midterm", score: 88, max: 100 }
-            ]
-        }
-    ],
+import courses from "./courses.json" with { type: "json" };
 
-    "Spring 2026": [
-        {
-            name: "S26C1", credit: 4, grade: "A+", assignments: [
-                { name: "Homework 1", score: 95, max: 100 },
-                { name: "Midterm", score: 88, max: 100 }
-            ]
-        }
-    ]
-};
+console.log(courses["Fall 2025"][0].name + " " + courses["Fall 2025"][0].grade);
