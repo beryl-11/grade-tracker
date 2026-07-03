8032858ee142c53152e15373299e23f126e8a298
beryl-11 <db.beryldong@gmail.com>
2026-07-03 12:22:45 -0400

    edited courses object

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-const courses = [
-    {
-        name: "Course 1",
-        credit: 4,
-        semester: "Fall 2025",
-        grade: "A+",
-        assignments: [
-            { name: "Homework 1", score: 95, max: 100 },
-            { name: "Midterm", score: 88, max: 100 }
-        ]
-    },
-    {
-        name: "Course 2",
-        credit: 4,
-        semester: "Spring 2026",
-        grade: "A+",
-        assignments: [
-            { name: "Homework 1", score: 95, max: 100 },
-            { name: "Midterm", score: 88, max: 100 }
-        ]
-    },
-    {
-        name: "Course 3",
-        credit: 2,
-        semester: "Fall 2025",
-        grade: "A-",
-        assignments: [
-            { name: "Homework 1", score: 95, max: 100 },
-            { name: "Midterm", score: 88, max: 100 }
-        ]
-    }
-];
+const courses = {
+    "Fall 2025": [
+        {
+            name: "F25C1", credit: 4, grade: "A+", assignments: [
+                { name: "Homework 1", score: 95, max: 100 },
+                { name: "Midterm", score: 88, max: 100 }
+            ]
+        },
+        {
+            name: "F25C1", credit: 2, grade: "A-", assignments: [
+                { name: "Homework 1", score: 95, max: 100 },
+                { name: "Midterm", score: 88, max: 100 }
+            ]
+        }
+    ],
+
+    "Spring 2026": [
+        {
+            name: "S26C1", credit: 4, grade: "A+", assignments: [
+                { name: "Homework 1", score: 95, max: 100 },
+                { name: "Midterm", score: 88, max: 100 }
+            ]
+        }
+    ],
+};
+
 const courseNames = document.getElementById("course-names");
 
 courses.forEach(course => {
