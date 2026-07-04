f0506fd4ef74c93178120fddf13a9f38d4b66795
beryl-11 <db.beryldong@gmail.com>
2026-07-04 11:45:07 -0400

    moved script to script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-import courses from "./courses.json" with { type: "json" };
+// import courses from "./courses.json" with { type: "json" };
 
-console.log(courses["Fall 2025"][0].name + " " + courses["Fall 2025"][0].grade);
+// console.log(courses["Fall 2025"][0].name + " " + courses["Fall 2025"][0].grade);
 
+/* Collapsible Content Script*/
+let collapsibles = document.querySelectorAll(".collapsible-btn");
+collapsibles.forEach((collapsible) => {
+    collapsible.addEventListener("click", function () {
+        collapsible.classList.toggle("expanded");
+        let content = collapsible.nextElementSibling;
+        content.classList.toggle("hidden");
+    })
+})
+
+/* Link Table Rows Using JQuery */
+$(document).ready(function () {
+    $(document.body).on("click", "tr[data-href]", function () {
+        window.location.href = this.dataset.href;
+    });
+});
