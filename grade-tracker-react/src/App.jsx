0cf2c0fa6c1f14187d5022ad3bbc719f8e2dfbed
beryl-11 <db.beryldong@gmail.com>
2026-07-12 17:08:20 -0400

    added global header bar component

--- grade-tracker-react/src/App.jsx
+++ grade-tracker-react/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import GlobalHeaderBar from './components/GlobalHeaderBar'
 import Home from './pages/Home'
 import GradeDetails from './pages/GradeDetails'
 
 function App() {
   return (
     <>
+    <GlobalHeaderBar/>
     <Home/>
     <GradeDetails/>
     </>
