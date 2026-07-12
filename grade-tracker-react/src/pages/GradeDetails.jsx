43d8ad20f9760b81b4f7277e2eabcf70680cbe87
beryl-11 <db.beryldong@gmail.com>
2026-07-12 17:03:03 -0400

    initial react setup for path routing

---
+++ grade-tracker-react/src/pages/GradeDetails.jsx
@@ -0,0 +1,7 @@
+function GradeDetails() {
+    return <section id="grade-details">
+        <h1>Details</h1>
+    </section>
+}
+
+export default GradeDetails
---
+++ grade-tracker-react/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+    return <section id="home">
+        <h1>Home</h1>
+    </section>
+}
+
+export default Home
