6ea9f05daf268732f111e1c714f5d0eab59a617e
beryl-11 <db.beryldong@gmail.com>
2026-07-14 17:35:27 -0400

    moved collapsible semester group styling to separate css file

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,11 +1,14 @@
+import "../css/Collapsible.css"
+import { Link } from "react-router-dom"
+
 function Collapsible() {
-    return <div className="collapsible semester-group">
+    return <div className="collapsible">
         <button type="button" className="collapsible-btn">Collapsible Button</button>
         <table className="collapsible-content">
-            <tr data-href="/details">
+            <Link to="/details"><tr>
                 <td className="course-name">course 1</td>
                 <td className="course-grade">A+</td>
-            </tr>
+            </tr></Link>
             <tr data-href="/details">
                 <td className="course-name">course 2</td>
                 <td className="course-grade">A</td>
