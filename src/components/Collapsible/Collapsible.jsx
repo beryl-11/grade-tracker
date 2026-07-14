bd6fa73363217c815309254510cbe0e153011d41
beryl-11 <db.beryldong@gmail.com>
2026-07-14 19:37:00 -0400

    home to collapsible connection established

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import ClickableTableRow from "./ClickableTableRow";
 
 function Collapsible(props) {
+    function collapsibleHandler() {
+        alert("collapse toggle")
+    }
+
     const { semester, courseList } = props;
 
     return <div className="collapsible">
-        <button type="button" className="collapsible-btn">{semester}</button>
+        <button type="button" className="collapsible-btn" onClick={collapsibleHandler}>{semester}</button>
         <table className="collapsible-content">
             <tbody>
                 {courseList.map((courseObject) => <ClickableTableRow key={courseObject.courseName} courseName={courseObject.courseName} courseGrade={courseObject.grade} />)}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,31 +2,37 @@ import Collapsible from "../components/Collapsible/Collapsible"
 // import courses from "../data/courses.json"
 
 function Home() {
-  const courses = {
-    "Fall 2025": [
-      {
-        "courseName": "F25C1", "credit": 4, "grade": "A+", "assignments": [
-          { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-          { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-        ]
-      },
-      {
-        "courseName": "F25C2", "credit": 2, "grade": "A-", "assignments": [
-          { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-          { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-        ]
-      }
-    ],
-
-    "Spring 2026": [
-      {
-        "courseName": "S26C1", "credit": 4, "grade": "A+", "assignments": [
-          { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-          { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-        ]
-      }
-    ]
-  }
+  const courses = [
+    {
+      semester: "Fall 2025",
+      courseList: [
+        {
+          "courseName": "F25C1: Introduction to Fall 2025", "credit": 4, "grade": "A+", "assignments":
+            [
+              { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+              { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+            ]
+        },
+        {
+          "courseName": "F25C2: Another Fall Semester Course", "credit": 2, "grade": "A-", "assignments": [
+            { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+            { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+          ]
+        }
+      ]
+    },
+    {
+      semester: "Spring 2026",
+      courseList: [
+        {
+          "courseName": "S26C1: First Course of the Spring Semester", "credit": 4, "grade": "A+", "assignments": [
+            { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+            { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+          ]
+        }
+      ]
+    }
+  ]
 
   return <section id="home">
     <header id="intro">
@@ -41,7 +47,7 @@ function Home() {
 
       <div className="grid-container" id="grade-details-grid">
         <div className="flex-container" id="details-list">
-          <Collapsible semester="Fall 2025" courseList={courses["Fall 2025"]}/>
+          {courses.map((semesterGroup) => <Collapsible semester={semesterGroup.semester} courseList={semesterGroup.courseList} />)}
         </div>
         <div className="graph-wrapper">
           <div className="circle-placeholder" id="gpa-breakdown-graph"></div>
