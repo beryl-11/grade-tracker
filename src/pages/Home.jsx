7a40e6ac27a514da9aeb35a33a6eceea4221577f
beryl-11 <db.beryldong@gmail.com>
2026-07-14 21:08:49 -0400

    change data source to json file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,38 @@
 import Collapsible from "../components/Collapsible/Collapsible"
-// import courses from "../data/courses.json"
+import courses from "../data/courses.json"
 
 function Home() {
-  const courses = [
-    {
-      semester: "Fall 2025",
-      courseList: [
-        {
-          "courseName": "F25C1: Introduction to Fall 2025", "credit": 4, "grade": "A+", "assignments":
-            [
-              { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-              { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-            ]
-        },
-        {
-          "courseName": "F25C2: Another Fall Semester Course", "credit": 2, "grade": "A-", "assignments": [
-            { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-            { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-          ]
-        }
-      ]
-    },
-    {
-      semester: "Spring 2026",
-      courseList: [
-        {
-          "courseName": "S26C1: First Course of the Spring Semester", "credit": 4, "grade": "A+", "assignments": [
-            { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
-            { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
-          ]
-        }
-      ]
-    }
-  ]
+  // const courses = [
+  //   {
+  //     semester: "Fall 2025",
+  //     courseList: [
+  //       {
+  //         "courseName": "F25C1: Introduction to Fall 2025", "credit": 4, "grade": "A+", "assignments":
+  //           [
+  //             { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+  //             { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+  //           ]
+  //       },
+  //       {
+  //         "courseName": "F25C2: Another Fall Semester Course", "credit": 2, "grade": "A-", "assignments": [
+  //           { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+  //           { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+  //         ]
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     semester: "Spring 2026",
+  //     courseList: [
+  //       {
+  //         "courseName": "S26C1: First Course of the Spring Semester", "credit": 4, "grade": "A+", "assignments": [
+  //           { "assignmentName": "Homework 1", "score": 95, "max": 100, "weight": 1 },
+  //           { "assignmentName": "Midterm", "score": 88, "max": 100, "weight": 1 }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ]
 
   return <section id="home">
     <header id="intro">
@@ -47,7 +47,7 @@ function Home() {
 
       <div className="grid-container" id="grade-details-grid">
         <div className="flex-container" id="details-list">
-          {courses.map((semesterGroup) => <Collapsible semester={semesterGroup.semester} courseList={semesterGroup.courseList} />)}
+          {courses.map((semesterGroup) => <Collapsible key={semesterGroup.semester} semester={semesterGroup.semester} courseList={semesterGroup.courseList} />)}
         </div>
         <div className="graph-wrapper">
           <div className="circle-placeholder" id="gpa-breakdown-graph"></div>
