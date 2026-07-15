a3b4efee15999c2d9887eaee16d421d34253a654
beryl-11 <db.beryldong@gmail.com>
2026-07-15 16:58:22 -0400

    use states to toggle collapsible

--- src/components/Collapsible/Collapsible.jsx
+++ src/components/Collapsible/Collapsible.jsx
@@ -1,18 +1,25 @@
 import "../../css/Collapsible.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import ClickableTableRow from "./ClickableTableRow";
 
 function Collapsible(props) {
-    // TODO: Make function toggle the collapsible (perhaps using states)
-    function collapsibleHandler() {
-        alert("collapse toggle")
+    const [collapsibleState, setCollapsibleState] = useState("collapsed");
+
+    //TODO: Find more elegant way of toggling states
+    function toggleCollapsible() {
+        if (collapsibleState === "collapsed") {
+            setCollapsibleState("expanded");
+        } else {
+            setCollapsibleState("collapsed");
+        }
     }
 
     const { semester, courseList } = props;
 
     return <div className="collapsible">
-        <button type="button" className="collapsible-btn" onClick={collapsibleHandler}>{semester}</button>
-        <table className="collapsible-content">
+        <button type="button" className="collapsible-btn" onClick={toggleCollapsible}>{semester}</button>
+        <table className={`collapsible-content ${collapsibleState}`}>
             <tbody>
                 {courseList.map((courseObject) => <ClickableTableRow key={courseObject.courseName} courseName={courseObject.courseName} courseGrade={courseObject.grade} />)}
             </tbody>
