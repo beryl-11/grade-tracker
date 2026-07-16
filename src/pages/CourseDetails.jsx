9586cef65624b5eea9cd1b6b16ece1c22eee7731
beryl-11 <db.beryldong@gmail.com>
2026-07-16 19:56:07 -0400

    course detail table in progress

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -5,11 +5,12 @@ export default function CourseDetails() {
     const params = useParams(); // TODO: Use the course ID to access the name, grade, and assignments associated with the course
     const semester = params.semester, courseId = params.courseId;
     const courseToDisplay = courses.find((semesterGroup) => semesterGroup.semester === semester).courseList.find((course) => course.courseId === courseId);
+    const { courseName, grade, assignments } = courseToDisplay
 
     return <section id="grade-details">
         <header>
-            <h1 className="page-title" id="course-view-name">{courseToDisplay.courseName}</h1>
-            <p className="overall-grade">Your estimated grade for this course is {courseToDisplay.grade}.</p>
+            <h1 className="page-title" id="course-view-name">{courseName}</h1>
+            <p className="overall-grade">Your estimated grade for this course is {grade}.</p>
         </header>
 
         <hr />
@@ -26,36 +27,15 @@ export default function CourseDetails() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Lab 0</td>
-                            <td>Lab</td>
-                            <td>1</td>
-                            <td>100%</td>
-                        </tr>
-                        <tr>
-                            <td>Lab 1</td>
-                            <td>Lab</td>
-                            <td>1</td>
-                            <td>100%</td>
-                        </tr>
-                        <tr>
-                            <td>Homework Assignment 1</td>
-                            <td>Homework</td>
-                            <td>1</td>
-                            <td>98%</td>
-                        </tr>
-                        <tr>
-                            <td>Quiz</td>
-                            <td>Assessments</td>
-                            <td>0.5</td>
-                            <td>80%</td>
-                        </tr>
-                        <tr>
-                            <td>Quiz</td>
-                            <td>Assessments</td>
-                            <td>0.5</td>
-                            <td>80%</td>
-                        </tr>
+                        {assignments.map((assignment, index) => {
+                            return <tr key={`${courseId}-${index}`}>
+                                <td>{assignment.assignmentName}</td>
+                                <td>{assignment.category}</td>
+                                <td>{assignment.weight}</td>
+                                <td>{assignment.score / assignment.max}</td>
+                            </tr>
+                        })
+                        }
                     </tbody>
                 </table>
             </div>
