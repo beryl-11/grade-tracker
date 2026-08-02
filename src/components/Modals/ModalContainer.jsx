057832e809538b16747c1838f2caff092b2063e5
beryl-11 <db.beryldong@gmail.com>
2026-08-02 17:39:34 -0400

    converted html for modals to modal components

--- src/components/Modals/ModalContainer.jsx
+++ src/components/Modals/ModalContainer.jsx
@@ -1,6 +1,6 @@
 import '../../css/modal.css';
 import { createPortal } from "react-dom";
-import {SettingsModal} from "./Modal";
+import {AddCourseModal, SettingsModal} from "./Modal";
 import { ModalContextProvider, useModalContext } from './ModalManager';
 
 export function ModalContainer() {
@@ -9,7 +9,7 @@ export function ModalContainer() {
         <>
             {/* TODO: Close modal from backdrop without having onClick read clicks from child elements */}
             {modalState==="open" && <div className='modal-backdrop'>
-                <SettingsModal />
+                <AddCourseModal />
             </div>}
         </>,
         document.body
