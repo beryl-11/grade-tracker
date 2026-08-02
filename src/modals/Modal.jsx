9d91be38416155fbf1f4958304b5e9ef9e842f08
beryl-11 <db.beryldong@gmail.com>
2026-08-02 17:15:05 -0400

    replaced class with className

--- src/modals/Modal.jsx
+++ src/modals/Modal.jsx
@@ -1,21 +1,21 @@
 import { useModalContext } from "../components/ModalManager"
 
 export function Modal() {
-    return <div class="modal" id="template-modal">
-        <button type="button" class="modal-exit-btn" aria-label="exit modal button">
+    return <div className="modal" id="template-modal">
+        <button type="button" className="modal-exit-btn" aria-label="exit modal button">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                class="icon">
+                className="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
         </button>
         {/* TODO: Delete the following dummy text and move them to a modal content component or something similar. */}
-        <div class="modal-header">
+        <div className="modal-header">
             <h1>Header</h1>
         </div>
 
         <hr />
 
-        <div class="modal-section">
+        <div className="modal-section">
             <h2>Random Heading</h2>
             <p>Some paragraph should be here.</p>
         </div>
