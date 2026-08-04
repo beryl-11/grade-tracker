4927a00227b17872d89d529fc384c04cde7ed3aa
beryl-11 <db.beryldong@gmail.com>
2026-08-04 18:33:38 -0400

    close modal with modal context using modalexitbutton

--- src/components/Modals/Modal.jsx
+++ src/components/Modals/Modal.jsx
@@ -1,90 +1,94 @@
+import { useModalContext } from "./ModalManager";
+
 function ModalTemplate({ children }) {
-    return <div className="modal">
-        <button type="button" className="modal-exit-btn" aria-label="exit modal button">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                className="icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-        </button>
-        {children}
-    </div>
+  const { closeModal } = useModalContext();
+
+  return <div className="modal">
+    <button type="button" className="modal-exit-btn" aria-label="exit modal button" onClick={closeModal}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        className="icon">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+      </svg>
+    </button>
+    {children}
+  </div>
 
 }
 
 export function SettingsModal() {
-    return <ModalTemplate id="settings-modal">
-        <div className="modal-header">
-            <h1>Settings</h1>
-        </div>
+  return <ModalTemplate id="settings-modal">
+    <div className="modal-header">
+      <h1>Settings</h1>
+    </div>
 
-        <hr />
+    <hr />
 
-        <div className="modal-section">
-            <h2>Random Heading</h2>
-            <p>There is nothing to tweak.</p>
-        </div>
-    </ModalTemplate>
+    <div className="modal-section">
+      <h2>Random Heading</h2>
+      <p>There is nothing to tweak.</p>
+    </div>
+  </ModalTemplate>
 }
 
 export function AddCourseModal() {
-    return <ModalTemplate id="add-course-modal">
-        <div className="modal-header">
-        <h1>Add Course</h1>
-        <p>You must complete all sections in order to submit this form.</p>
+  return <ModalTemplate id="add-course-modal">
+    <div className="modal-header">
+      <h1>Add Course</h1>
+      <p>You must complete all sections in order to submit this form.</p>
+    </div>
+
+    <hr />
+
+    <form id="add-course-form">
+      <div className="modal-section">
+        <h2>Basic Course Information</h2>
+        <label for="course-code">Course Code: </label>
+        <input type="text" name="courseCode" id="course-code" required />
+
+        <label for="course-name">Course Name: </label>
+        <input type="text" name="courseName" id="course-name" required />
+
+        <br />
+
+        <label for="semester">Semester: </label>
+        <select name="semester" id="semester" required>
+          <option>Fall 2026</option>
+          <option>Spring 2025</option>
+        </select>
+
+        <label for="credits">Number of Credits: </label>
+        <input type="number" name="credits" id="credits" required />
+      </div>
+      <div className="modal-section">
+        <h2>Add Components</h2>
+        <p>The weight of your components should add to 100%.</p>
+
+        <label for="component-name">Component Name: </label>
+        <input type="text" name="componentName" id="component-name" multiple />
+        <label for="component-weight">Component Weight: </label>
+        <input type="number" name="componentWeight" id="component-weight" /><br />
+        <button type="button">Add Component</button>
+
+        <h3>You've added:</h3>
+        <table className="modal-table" id="components-table-view">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Component Name</th>
+              <th>Component Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Total: </td>
+              <td></td>
+              <td>0%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
-      <hr />
-
-      <form id="add-course-form">
-        <div className="modal-section">
-          <h2>Basic Course Information</h2>
-          <label for="course-code">Course Code: </label>
-          <input type="text" name="courseCode" id="course-code" required />
-
-          <label for="course-name">Course Name: </label>
-          <input type="text" name="courseName" id="course-name" required />
-
-          <br />
-
-          <label for="semester">Semester: </label>
-          <select name="semester" id="semester" required>
-            <option>Fall 2026</option>
-            <option>Spring 2025</option>
-          </select>
-
-          <label for="credits">Number of Credits: </label>
-          <input type="number" name="credits" id="credits" required />
-        </div>
-        <div className="modal-section">
-          <h2>Add Components</h2>
-          <p>The weight of your components should add to 100%.</p>
-
-          <label for="component-name">Component Name: </label>
-          <input type="text" name="componentName" id="component-name" multiple />
-          <label for="component-weight">Component Weight: </label>
-          <input type="number" name="componentWeight" id="component-weight" /><br />
-          <button type="button">Add Component</button>
-
-          <h3>You've added:</h3>
-          <table className="modal-table" id="components-table-view">
-            <thead>
-              <tr>
-                 <th></th>
-                <th>Component Name</th>
-                <th>Component Weight</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Total: </td>
-                <td></td>
-                <td>0%</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <button type="submit">Add Course</button>
-      </form>
-    </ModalTemplate>
+      <button type="submit">Add Course</button>
+    </form>
+  </ModalTemplate>
 }
