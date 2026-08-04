a7d8505f945f9ae124e0c21e60b8ac82945e3167
beryl-11 <db.beryldong@gmail.com>
2026-08-04 18:39:27 -0400

    changed body to overlaysDiv

--- src/components/Modals/ModalContainer.jsx
+++ src/components/Modals/ModalContainer.jsx
@@ -1,18 +1,20 @@
 import '../../css/modal.css';
 import { createPortal } from "react-dom";
-import {AddCourseModal, SettingsModal} from "./Modal";
+import { AddCourseModal, SettingsModal } from "./Modal";
 import { ModalContextProvider, useModalContext } from './ModalManager';
 
 export function ModalContainer() {
-    const {modalState} = useModalContext();
+    const { modalState } = useModalContext();
+    const overlaysDiv = document.getElementById("overlays");
+
     return createPortal(
         <>
             {/* TODO: Close modal from backdrop without having onClick read clicks from child elements */}
-            {modalState==="open" && <div className='modal-backdrop'>
+            {modalState === "open" && <div className='modal-backdrop'>
                 <AddCourseModal />
             </div>}
         </>,
-        document.body
+        overlaysDiv
     )
 
 }
