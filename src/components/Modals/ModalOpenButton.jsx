bd7af6d5a8856a1fc495d5620fa824f086f13688
beryl-11 <db.beryldong@gmail.com>
2026-08-04 19:51:08 -0400

    using phosphor icons library

--- src/components/Modals/ModalOpenButton.jsx
+++ src/components/Modals/ModalOpenButton.jsx
@@ -1,12 +1,15 @@
 import { useModalContext } from "./ModalManager";
+import { QuestionIcon } from "@phosphor-icons/react";
 
-export default function ModalOpenButton({modalRef, textContent = ""}) {
-    const {openModal} = useModalContext();
+export default function ModalOpenButton({ modalRef, textContent = "" }) {
+    const { openModal } = useModalContext();
 
     const openBtnHandler = () => {
         openModal("open");
     }
 
-    return <button type="button" className="modal-open-btn" onClick={openBtnHandler}>Open
+    return <button type="button" className="modal-open-btn" onClick={openBtnHandler}>
+        <QuestionIcon size={24} className="icon"/>      
+        {textContent}
     </button>
 }
